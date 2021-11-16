dcf794f880e93c1de3b67e908371dcef406f69e1
Patrick Möller <patrick_moeller@live.de>
2021-11-16 13:13:07 +0100

    Adding Home and Search Button Syntax

--- src/app/components/ButtonSearch/ButtonSearch.tsx
+++ src/app/components/ButtonSearch/ButtonSearch.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function ButtonHome(
+export default function ButtonSearch(
   props: React.SVGProps<SVGSVGElement>
 ): JSX.Element {
   return (
@@ -16,7 +16,7 @@ export default function ButtonHome(
         stroke-linecap="round"
         stroke-linejoin="round"
         stroke-width="2"
-        d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
       />
     </svg>
   );
